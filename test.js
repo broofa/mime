73a461d8287e56a6f36c746da215eea98e4d551f
Benjamin Thomas <benjamin@benjaminthomas.org>
2010-02-24 19:09:14 -0700

    Make tests work, change exports slightly.

--- test.js
+++ test.js
@@ -1,30 +1,30 @@
 var async_testing = require('async_testing');
-var utils = require('../lib/utils');
+var mime = require('./mime');
 
 exports['Mime Tests'] = (new async_testing.TestSuite())
   .addTests({
     "test mime lookup": function(assert) {
       // easy
-      assert.equal('text/plain', utils.mime.lookup('text.txt'));
+      assert.equal('text/plain', mime.lookup('text.txt'));
       
       // just an extension
-      assert.equal('text/plain', utils.mime.lookup('.txt'));
+      assert.equal('text/plain', mime.lookup('.txt'));
       
       // default
-      assert.equal('application/octet-stream', utils.mime.lookup('text.nope'));
+      assert.equal('application/octet-stream', mime.lookup('text.nope'));
 
       // fallback
-      assert.equal('fallback', utils.mime.lookup('text.fallback', 'fallback'));
+      assert.equal('fallback', mime.lookup('text.fallback', 'fallback'));
     },
     "test charset lookup": function(assert) {
       // easy
-      assert.equal('UTF-8', utils.charsets.lookup('text/plain'));
+      assert.equal('UTF-8', mime.charsets.lookup('text/plain'));
       
       // none
-      assert.ok(typeof utils.charsets.lookup('text/nope') == 'undefined');
+      assert.ok(typeof mime.charsets.lookup('text/nope') == 'undefined');
 
       // fallback
-      assert.equal('fallback', utils.charsets.lookup('text/fallback', 'fallback'));
+      assert.equal('fallback', mime.charsets.lookup('text/fallback', 'fallback'));
     }
   });
 
