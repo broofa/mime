3b6031dc0b95033e57ed07270154c6dc5fe7859f
Benjamin Thomas <benjamin@benjaminthomas.org>
2010-08-11 00:21:37 -0600

    A couple small fixes
    + Make pulling the extension out of the filename smarter
    + Handle upper case or mixed case file names
    + Remove duplicates
    + Can pass in just the extension (no '.') and it will still work.

--- test.js
+++ test.js
@@ -1,33 +1,59 @@
-var async_testing = require('async_testing');
 var mime = require('./mime');
 
-exports['Mime Tests'] = (new async_testing.TestSuite())
-  .addTests({
-    "test mime lookup": function(assert) {
-      // easy
-      assert.equal('text/plain', mime.lookup('text.txt'));
-      
-      // just an extension
-      assert.equal('text/plain', mime.lookup('.txt'));
-      
-      // default
-      assert.equal('application/octet-stream', mime.lookup('text.nope'));
+exports["test mime lookup"] = function(test) {
+  // easy
+  test.equal('text/plain', mime.lookup('text.txt'));
+  
+  // hidden file or multiple periods
+  test.equal('text/plain', mime.lookup('.text.txt'));
 
-      // fallback
-      assert.equal('fallback', mime.lookup('text.fallback', 'fallback'));
-    },
-    "test charset lookup": function(assert) {
-      // easy
-      assert.equal('UTF-8', mime.charsets.lookup('text/plain'));
-      
-      // none
-      assert.ok(typeof mime.charsets.lookup('text/nope') == 'undefined');
+  // just an extension
+  test.equal('text/plain', mime.lookup('.txt'));
+  
+  // just an extension without a dot
+  test.equal('text/plain', mime.lookup('txt'));
+  
+  // default
+  test.equal('application/octet-stream', mime.lookup('text.nope'));
 
-      // fallback
-      assert.equal('fallback', mime.charsets.lookup('text/fallback', 'fallback'));
-    }
-  });
+  // fallback
+  test.equal('fallback', mime.lookup('text.fallback', 'fallback'));
 
-if (module === require.main) {
-  async_testing.runSuites(exports);
+  test.finish();
+};
+
+exports["test mime lookup uppercase"] = function(test) {
+  // easy
+  test.equal('text/plain', mime.lookup('TEXT.TXT'));
+  
+  // just an extension
+  test.equal('text/plain', mime.lookup('.TXT'));
+  
+  // just an extension without a dot
+  test.equal('text/plain', mime.lookup('TXT'));
+  
+  // default
+  test.equal('application/octet-stream', mime.lookup('TEXT.NOPE'));
+
+  // fallback
+  test.equal('fallback', mime.lookup('TEXT.FALLBACK', 'fallback'));
+
+  test.finish();
+};
+
+exports["test charset lookup"] = function(test) {
+  // easy
+  test.equal('UTF-8', mime.charsets.lookup('text/plain'));
+  
+  // none
+  test.ok(typeof mime.charsets.lookup('text/nope') == 'undefined');
+
+  // fallback
+  test.equal('fallback', mime.charsets.lookup('text/fallback', 'fallback'));
+
+  test.finish();
+};
+
+if (module == require.main) {
+  require('async_testing').run(__filename, process.ARGV);
 }
