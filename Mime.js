4bdbf762b62ceb2d846d583fe6d1dc1e46d733a3
Robert Kieffer <robert@broofa.com>
2017-08-16 15:42:29 -0700

    Mime -> es6 class

--- Mime.js
+++ Mime.js
@@ -2,25 +2,26 @@
  * @param typeMap [Object] Map of MIME type -> Array[extensions]
  * @param ...
  */
-function Mime() {
-  this._types = Object.create(null);
-  this._extensions = Object.create(null);
+class Mime {
+  constructor() {
+    this._types = Object.create(null);
+    this._extensions = Object.create(null);
 
-  for (var i = 0; i < arguments.length; i++) {
-    this.define(arguments[i]);
+    for (var i = 0; i < arguments.length; i++) {
+      this.define(arguments[i]);
+    }
   }
-}
 
-/**
- * Define mimetype -> xtension mappings.  Each key is a mime-type that maps
- * to an array of extensions associated with the type.  The first extension is
- * used as the default extension for the type.
- *
- * e.g. mime.define({'audio/ogg', ['oga', 'ogg', 'spx']});
- *
- * @param map (Object) type definitions
- */
-Mime.prototype.define = function(typeMap, force) {
+  /**
+   * Define mimetype -> xtension mappings.  Each key is a mime-type that maps
+   * to an array of extensions associated with the type.  The first extension is
+   * used as the default extension for the type.
+   *
+   * e.g. mime.define({'audio/ogg', ['oga', 'ogg', 'spx']});
+   *
+   * @param map (Object) type definitions
+   */
+  define(typeMap, force) {
     for (let type in typeMap) {
       var extensions = typeMap[type];
       for (let i = 0; i < extensions.length; i++) {
@@ -39,11 +40,10 @@ Mime.prototype.define = function(typeMap, force) {
     }
   }
 
-
-/**
- * Lookup a mime type based on extension
- */
-Mime.prototype.getType = function(path) {
+  /**
+   * Lookup a mime type based on extension
+   */
+  getType(path) {
     path = String(path);
     var last = path.replace(/.*[\/\\]/, '').toLowerCase();
     var ext = last.replace(/.*\./, '').toLowerCase();
@@ -54,12 +54,13 @@ Mime.prototype.getType = function(path) {
     return (hasDot || !hasPath) && this._types[ext] || null;
   }
 
-/**
- * Return file extension associated with a mime type
- */
-Mime.prototype.getExtension = function(type) {
-  type = /^\s*([^;\s]*)/.test(type) && RegExp.$1;
-  return type && this._extensions[type.toLowerCase()] || null;
+  /**
+   * Return file extension associated with a mime type
+   */
+  getExtension(type) {
+    type = /^\s*([^;\s]*)/.test(type) && RegExp.$1;
+    return type && this._extensions[type.toLowerCase()] || null;
+  }
 }
 
 module.exports = Mime;
