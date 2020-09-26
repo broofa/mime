7cd0851d83b7fa42ee0d7afab3736818de202ac6
Yash Singh <53054099+Yash-Singh1@users.noreply.github.com>
2020-09-25 19:44:10 -0700

    Added in version flag and name flag

--- cli.js
+++ cli.js
@@ -2,7 +2,18 @@
 
 'use strict';
 
+process.title = 'mime';
 var mime = require('.');
+var pkg = require('./package.json');
+var args = process.argv.splice(2);
+if (args.includes('--version') || args.includes('-v')) {
+  console.log(pkg.version);
+  process.exit(0);
+}
+else if (args.includes('--name')) {
+  console.log('mime');
+  process.exit(0);
+}
 var file = process.argv[2];
 var type = mime.getType(file);
 
