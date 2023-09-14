88a4addbecbae8e57ae2f9c4a7e92c70223b0cb3
Robert Kieffer <robert@broofa.com>
2023-09-13 21:15:23 -0700

    patch: fixup Mime import paths

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,6 @@ import otherTypes from '../types/other.js';
 import standardTypes from '../types/standard.js';
 import Mime from './Mime.js';
 
+export { default as Mime } from './Mime.js';
+
 export default new Mime().define(standardTypes).define(otherTypes)._freeze();
--- src/index_lite.ts
+++ src/index_lite.ts
@@ -2,3 +2,5 @@ import standardTypes from '../types/standard.js';
 import Mime from './Mime.js';
 
 export default new Mime().define(standardTypes)._freeze();
+
+export { default as Mime } from './Mime.js';
