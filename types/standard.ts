65f82454be582cbb47fb4ec85534a0525f3c8f1a
Matt Woolford <57449285+mattwoolford@users.noreply.github.com>
2025-09-11 13:03:51 +0100

    Update standard.ts
    - Enable literal types on the type map object so that MIME types are accessible in Typescript.
    - Preserve type safety of `{[key: string]: string}` using Typescript `satisfies`

--- types/standard.ts
+++ types/standard.ts
@@ -1,4 +1,4 @@
-const types: { [key: string]: string[] } = {
+const types = {
   'application/andrew-inset': ['ez'],
   'application/appinstaller': ['appinstaller'],
   'application/applixware': ['aw'],
@@ -361,6 +361,6 @@ const types: { [key: string]: string[] } = {
   'video/ogg': ['ogv'],
   'video/quicktime': ['qt', 'mov'],
   'video/webm': ['webm'],
-};
+} satisfies Readonly<{ [key: string]: string[] }>;
 Object.freeze(types);
 export default types;
